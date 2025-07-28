47520ca92b6f942a7896788283a08af778c310b0
Mornac <imornac@gmail.com>
2025-07-28 16:01:02 +0000

    Function concats 2 strings and returns a concatenated string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Concat 2 strings as arguments
+Params:
+    str1: string
+    str2: string
+Return a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Definition of function concat of 2 strings
+    """
+    return str1 + str2

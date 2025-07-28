7e0aa53ca7eaa4c49cacdcc6e626c94fd5bc9912
Mornac <imornac@gmail.com>
2025-07-28 15:36:39 +0000

    Function returns the sum of 2 floats

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+Returns the sum of 2 floats
+Params: 
+    a: float
+    b: float
+Return: a + b
+"""
+def add(a, b):  
+    a = float,
+    b = float,
+
+    return (a + b)
+

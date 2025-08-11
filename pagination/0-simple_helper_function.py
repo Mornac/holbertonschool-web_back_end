2ce3376bb7d4481d2531bec4cb9af196ad1ab13f
Mornac <imornac@gmail.com>
2025-08-11 13:17:22 +0200

    Change the return to add the page size according to the page

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,15 +4,14 @@ Function that should return a tuple of size to containing
 a start index and an end index corresponding to the range of indexes
 to return in a list for those particular pagination parameters
 """
+from typing import Union, Tuple
 
-
-def index_range(page, page_size):
+def index_range(page, page_size) -> Union[tuple[int, int], None]:
     """
     Params:
         page: index of the page
         page_size: number of elements in the page
     Return a start index and an end index in a list
     """
-    page += 0;
 
-    return (page, page_size)
+    return (page * page_size - page_size, page * page_size)

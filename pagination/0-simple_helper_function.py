f62b1dba2c34dd622be76c24b1d83722190c73b4
Mornac <imornac@gmail.com>
2025-08-11 13:06:04 +0200

    function named index_range that takes two integer arguments page and page_size and the main associated

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Function that should return a tuple of size to containing
+a start index and an end index corresponding to the range of indexes
+to return in a list for those particular pagination parameters
+"""
+
+
+def index_range(page, page_size):
+    """
+    Params:
+        page: index of the page
+        page_size: number of elements in the page
+    Return a start index and an end index in a list
+    """
+    
+    return (page, page_size)

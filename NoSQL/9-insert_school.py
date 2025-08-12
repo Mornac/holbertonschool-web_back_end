4042344b3f14fbcfc354fd340803e5a56f694f4e
Mornac <imornac@gmail.com>
2025-08-12 12:24:32 +0200

    Insert a new document in a collection

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Function that inserts a new document in a collection based on kwargs
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection
+    pymongo collection object: mongo_collection
+    Return: new _id
+    """
+    return mongo_collection.insert_one(kwargs).inserted_id

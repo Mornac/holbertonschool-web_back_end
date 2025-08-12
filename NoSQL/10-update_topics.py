c3ebeed8ee4364a7130fe1524565588ab6772622
Mornac <imornac@gmail.com>
2025-08-12 12:33:52 +0200

    changes all topics of a school document based on a name

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Function that changes all topics of a school document
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name:
+    pymongo collection object: mongo_collection
+    name: school name to update, string
+    topics: list of topics approached in the school, list of strings
+    """
+    return mongo_collection.update_many(name, topics)

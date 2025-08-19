f05ca7fd50e1278cac0f6535152690dd2af2d54e
Mornac <imornac@gmail.com>
2025-08-19 14:28:14 +0200

    Implement a classe with 2 constructor attributes

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,31 @@
+export default class Currency {
+    constructor(code, name) {
+        this._code = code;
+        this._name = name;
+        
+    }
+
+    get code() {
+        return this._code;
+    }
+    set code(newcode) {
+        if (typeof newcode !== 'string') {
+            throw new TypeError('code must be a string');
+        }
+        this._code = newcode;
+    }
+
+    get name() { 
+        return this._name;
+    }
+    set name(newname) {
+        if (typeof newname !== 'string') {
+            throw new TypeError('name must be a string');
+        }
+        this._name = newname;
+    }
+
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`;
+    }
+}

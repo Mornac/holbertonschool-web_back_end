9763ef4659136f4f36aeee14331eaa99cd0686a5
Mornac <imornac@gmail.com>
2025-08-19 15:17:00 +0200

    Implement a class extended and a method

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,18 @@
+import building from './5-building.js';
+
+export default class SkyHighBuilding extends building {
+    constructor(sqft, floors) {
+        super(sqft)
+        if (typeof floors !== 'number')
+            throw new TypeError('floors must be a number');
+        this._floors = floors;
+    }
+
+    get floors() {
+        return this._floors;
+    }
+    
+    evacuationWarningMessage() {
+        return `Evacuate slowly the ${this._floors} floors`;
+    }
+}

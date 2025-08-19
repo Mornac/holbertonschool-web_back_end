3d555ebd551d42616571fcde987b8be09cca7591
Mornac <imornac@gmail.com>
2025-08-19 11:10:49 +0200

    Add a comment and put the return after the export

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,13 +1,13 @@
+/*eslint-disable*/
 function getCurrentYear() {
     const date = new Date();
-  return date.getFullYear();
+    return date.getFullYear();
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-    const budget = {
-        [`income-${getCurrentYear()}`]: income,
-        [`gdp-${getCurrentYear()}`]: gdp,
-        [`capita-${getCurrentYear()}`]: capita,
+    return{
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
     };
-  return budget;
 }

93f36795021e5890d0ac7c74fe60fc2d9466404b
mornac <imornac@gmail.com>
2025-08-25 12:36:08 +0200

    Create a function that returns the sum of all the student ids and test

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+import getListStudentIds from './1-get_list_student_ids';
+
+export default function getStudentIdsSum(students) {
+  const ids = getListStudentIds(students);
+  return ids.reduce((a, b) => a + b, 0);
+}
---
+++ ES6_data_manipulation/tests/3-get_ids_sum.test.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import getStudentIdsSum from '../3-get_ids_sum';
+
+describe("getStudentsIdsSum", () => {
+  test("should return a sum of Ids", () => {
+    const ids = getStudentIdsSum();
+    expect(ids).toBe(ids);
+  });
+});

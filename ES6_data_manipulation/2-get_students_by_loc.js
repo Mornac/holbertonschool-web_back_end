ca6e3fc7e30e17e8141a29926b7ef1a404d517f1
mornac <imornac@gmail.com>
2025-08-26 09:02:07 +0200

    Add a semicolon at the end of the code

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -4,4 +4,4 @@ export default function getStudentsByLocation() {
     {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
     {id: 5, firstName: 'Serena', location: 'San Francisco'}
   ];
-}
+};

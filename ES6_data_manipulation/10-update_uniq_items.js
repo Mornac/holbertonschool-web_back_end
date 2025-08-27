0308820cb7e7850679ccbeb3864c346e218475bc
mornac <imornac@gmail.com>
2025-08-27 13:27:57 +0200

    Add const to the code

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,16 @@
 /* eslint-disable */
 export default function updateUniqueItems(map) {
-  /* if (!(map instanceof Map)) {
+  if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-*/
-  for (const [keys, value] of map.entries()) {
+  for (const entry of map) {
+    const key = entry[0];
+    const value = entry[1];
+
     if (value === 1) {
-      map.set(keys, 100);
+      map.set(key, 100);
     }
   }
+
+  return map;
 }

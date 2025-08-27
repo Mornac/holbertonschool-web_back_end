e476d8a17b34d53a92728bc0a224a279d1482c24
mornac <imornac@gmail.com>
2025-08-27 11:23:30 +0200

    Try to delete the uppercase in the arguments

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,10 +2,10 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstname, lastname, filename) {
   return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
+    signUpUser(firstname, lastname),
+    uploadPhoto(filename),
   ])
   .then((results) => {
     return results.map((result) => {

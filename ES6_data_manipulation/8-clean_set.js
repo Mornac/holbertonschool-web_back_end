8b4c3caa0ac0e341df0c85b00085d925b3f8e358
mornac <imornac@gmail.com>
2025-08-27 12:09:20 +0200

    Delete set in the arguments of function

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
 /* eslint-disable */
-export default function cleanSet(set, startString) {
-  if (startString.length === 0) return '';
-  const result = [];
-    for (const item of set) {
-      if (item.startsWith(startString)) {
-      result.push(item.slice(startString.length));
-      }
-    }
-  return result.join('-');
-}
+export default function cleanSet(startString) {
+  if (typeof startString !== 'string' || startString.length === 0) 
+    return '';
+  }
+
+  const strings = [...set]
+    .filter((value) => value && value.startsWith(startString))
+    .map((value) => value.slice(startString.length));
+  
+  return strings.join('-');

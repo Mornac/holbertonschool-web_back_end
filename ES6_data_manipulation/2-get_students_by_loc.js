22b403dd97183beddcb7685bf69441e4c8ebf520
mornac <imornac@gmail.com>
2025-08-27 13:55:30 +0200

    Identify the name of the city

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
-export default function getStudentsByLocation(students, city) {
+export default function getStudentsByLocation(students, city='San Francisco') {
   return students.filter(student => student.location === city);
 }

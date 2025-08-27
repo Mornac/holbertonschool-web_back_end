15a452ed0382c54bb64d438790f91e369e56f61c
mornac <imornac@gmail.com>
2025-08-27 10:57:15 +0200

    Add async function and await

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,15 +2,22 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = await Promise.allSettled([
     signUpUser (firstName, lastName),
     uploadPhoto (fileName),
   ])
-  .then((results) => 
-    results.map((result) => ({
-      status: result.status,
-      value: result.status ==='fulfilled' ? result.value : String(result.reason),
-    }))
-  );
+  return results.map((result) => {
+    if (result.status === 'fulfilled') {
+      return {
+        status: result.status,
+        value: result.value,
+      };
+    } else {
+      return {
+        status: result.status,
+        value : String(result.reason),
+      };
+    }
+  })
 }

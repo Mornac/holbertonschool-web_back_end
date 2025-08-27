b63e29b98bcc18c8a0936ed2e6b2ede45aa78ee6
mornac <imornac@gmail.com>
2025-08-27 08:59:42 +0200

    Resolve all promises and log body, firstname and lastname to the console

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,17 +1,12 @@
 /* eslint-disable */
-import { utils } from "jest-snapshot";
-import uploadPhoto from utils.js
-import createUser from utils.js
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const [photo, user] = results;
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log("Signup system offline");
-    })
-    .finally(() => {
-      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-    })
+    });
 }

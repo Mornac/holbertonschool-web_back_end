b138be005081c57c57d593fd8b3a756e661bd02b
mornac <imornac@gmail.com>
2025-08-27 09:13:00 +0200

    Returns a resolved promise with object

---
+++ ES6_promise/4-user-promise.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+
+export default function signUpUser(firstName, lastName) {
+  return new Promise((resolve) => {
+    const user = { firstName, lastName };
+    resolve(user);
+  });
+}

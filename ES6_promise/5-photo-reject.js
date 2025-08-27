34daa228098588a663a53bdae01677962730bca7
mornac <imornac@gmail.com>
2025-08-27 09:23:33 +0200

    Function that returns a promise rejecting with an error and a string

---
+++ ES6_promise/5-photo-reject.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+export default function uploadPhoto(fileName) {
+  return new Promise((reject) => {
+    reject(new Error('<rejected> Error: $fileName cannot be processed)'));
+  });
+};

a25e4657283a47aefd1442923f82459b218ac417
mornac <imornac@gmail.com>
2025-08-27 13:14:48 +0200

    Replace the arg of the function

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,10 +1,12 @@
 /* eslint-disable */
 export default function updateUniqueItems(map) {
-  const newMap = new Map(map);
-  for (const [key, value] of newMap) {
+  if (!(map instanceof Map)) {
+    throw new Error ('Cannot process');
+  }
+
+  for (const [key, value] of map.entries()) {
     if (value === 1) {
-      newMap.set(key, 100);
+      map.set(key, 100);
     }
   }
-  return newMap;
 }

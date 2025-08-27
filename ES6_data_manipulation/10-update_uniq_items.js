f054b12d93dbc771710f0021a17001a33061106f
mornac <imornac@gmail.com>
2025-08-27 13:20:32 +0200

    Cancel the first if

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default function updateUniqueItems(map) {
-  if (!(map instanceof Map)) {
+  /* if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-
+*/
   for (const [keys, value] of map.entries()) {
     if (value === 1) {
       map.set(keys, 100);

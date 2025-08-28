9716a624026787a4ac6ac5b162838291eaa933ea
mornac <imornac@gmail.com>
2025-08-28 09:03:47 +0200

    Delete the condition if

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,3 @@
 export default function getStudentsByLocation(getListStudents, city) {
-  if (Array.isArray(getListStudents)) {
-    return getListStudents.filter((student) => student.location === city);
-  }
-  return [];
+  return getListStudents.filter((student) => student.location === city);
 }

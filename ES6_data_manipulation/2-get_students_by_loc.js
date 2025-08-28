eedb7784c3bca3f7f57893193c1446025ae07c3b
mornac <imornac@gmail.com>
2025-08-28 08:53:56 +0200

    Add a condition for array

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,6 @@
 export default function getStudentsByLocation(getListStudents, city='San Francisco') {
-  return getListStudents.filter(student => student.location === city);
+  if (Array.isArray(getListStudents)) {
+    return getListStudents.filter(student => student.location === city);
+  }
+  return [];
 }

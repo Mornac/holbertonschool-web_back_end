1dde42b125a9dcbc6acaae1d31a3e43abd744283
mornac <imornac@gmail.com>
2025-08-28 10:18:11 +0200

    Add the async function

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstname, lastname, filename) {
+export default async function handleProfileSignup(firstname, lastname, filename) {
   return Promise.allSettled([
     signUpUser(firstname, lastname),
     uploadPhoto(filename),

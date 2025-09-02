f4f859f55c7739ae8affeb9d4a819741797c56e0
mornac <imornac@gmail.com>
2025-09-02 17:39:33 +0200

    Add the const port

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,9 +1,11 @@
 const http = require('http');
+const port = 1245;
 
 const app = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain' });
   response.end('Hello Holberton School!');
   })
-  app.listen(1245);
+  
+  app.listen(port);
 
 module.exports = app;

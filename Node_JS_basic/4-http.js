946d149155f6d8fb092489918cbd46138c4c6b55
mornac <imornac@gmail.com>
2025-09-02 17:18:38 +0200

    Fix: writeHead

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,10 +1,9 @@
 const http = require('http');
 
-http.createServer((request, response) => {
-  response.write('Hello Holberton School!');
-  response.end();
-}).listen(1245), () => {
-    console.log('Server running at http://localhost:1245/');
-};
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end('Hello Holberton School!');
+  })
+  app.listen(1245);
 
 module.exports = app;

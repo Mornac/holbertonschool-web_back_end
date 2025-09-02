0e16dd300b6c5f7f73497a2f4d19b7cbfcef45a9
mornac <imornac@gmail.com>
2025-09-02 17:56:46 +0200

    change the sens of the conditions

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,20 +4,16 @@ const countStudents = require('./3-read_file_async');
 const app = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain' });
 
-  if (request.url === '/') {
-    response.end('Hello Holberton School!');
-  } else if (request.url === '/students') {
-    const database = process.argv[2];
-    countStudents(database)
+  if (request.url === '/students') {
+    countStudents(process.argv[2])
       .then((output) => {
         response.end(`This is the list of our students\n${output}`);
       })
       .catch((err) => {
-        response.end(`This is the list of our students\n${err.message}`);
+        response.end(err.message);
       });
   } else {
-    response.writeHead(404);
-    response.end('Not found');
+    response.end('Hello Holberton School!');
   }
 });
 

dc232a52053ff4e5ab4fe2d1bfbd27ee3e606216
mornac <imornac@gmail.com>
2025-09-04 13:49:39 +0200

    Fix error arg to argv

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -4,19 +4,16 @@ const countStudents = require('./3-read_file_async');
 const app = express();
 
 app.get('/', (request, response) => {
-//  response.set('Content-Type', 'text/plain');
   response.send('Hello Holberton School!');
 });
 
 app.get('/students', (request, response) => {
-  countStudents(process.arg[2])
+  countStudents(process.argv[2])
     .then((output) => {
-      //      response.set('Content-Type', 'text/plain');
-      response.send(`This is the list of our students\n${output}`);
+        response.send(`This is the list of our students\n${output}`);
     })
     .catch((err) => {
-      //      response.set('Content-Type', 'text/plain');
-      response.send(err.message);
+        response.send(`This is the list of our students\n${err.message}`);
     });
 });
 
